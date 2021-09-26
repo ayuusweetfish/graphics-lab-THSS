82e48bcaec0b81cb453325770d15216afb07e751
Shiqing <shiqing-thu18@yandex.com>
2021-09-26 23:43:52 +0800

    Properly implement pointer events on canvas

--- src/app.rs
+++ src/app.rs
@@ -6,6 +6,10 @@ use eframe::{egui, epi};
 pub struct App {
   dark: bool,
   polygon_khroma: [f32; 3],
+
+  last_rect_popup: egui::Rect,
+  last_pt_down: (bool, bool),
+  last_pt_held: (bool, bool),
 }
 
 impl Default for App {
@@ -13,6 +17,10 @@ impl Default for App {
     Self {
       dark: false,
       polygon_khroma: [0f32; 3],
+
+      last_rect_popup: egui::Rect::NOTHING,
+      last_pt_down: (false, false),
+      last_pt_held: (false, false),
     }
   }
 }
@@ -27,16 +35,48 @@ impl App {
 
   fn process_canvas_interactions(
     &mut self,
-    painter: egui::Painter, rect: egui::Rect, resp: egui::Response,
+    painter: egui::Painter,
+    rect: egui::Rect,
+    exclude: egui::Rect,
+    resp: egui::Response,
+    input: &egui::InputState,
   ) {
-    if let Some(p) = resp.hover_pos().or(resp.interact_pointer_pos()) {
-      painter.add(egui::Shape::circle_filled(p, 10.0,
-        if resp.is_pointer_button_down_on() {
-          egui::Color32::from_rgb(255, 128, 128)
-        } else {
-          egui::Color32::from_rgb(255, 255, 128)
-        }));
-    }
+    let pt_pos = match input.pointer.hover_pos().or(input.pointer.interact_pos()) {
+      Some(p) => p,
+      None => return, // Return if no position information is available
+    };
+
+    // Button states: buttons themselves
+    let (last_pt1, last_pt2) = self.last_pt_down;
+    let pt1 = input.pointer.primary_down();
+    let pt2 = input.pointer.secondary_down();
+    self.last_pt_down = (pt1, pt2);
+
+    // Whether buttons are pressed (up -> down)
+    let pt1_press = !last_pt1 && pt1;
+    let pt2_press = !last_pt2 && pt2;
+    // Whether buttons are released (down -> up)
+    let (last_pt1_held, last_pt2_held) = self.last_pt_held;
+    let pt1_rel = last_pt1_held && !pt1;
+    let pt2_rel = last_pt2_held && !pt2;
+
+    // Hold states: valid button presses and drags in the region
+    let excluded = exclude.contains(pt_pos) || !rect.contains(pt_pos);
+    if !pt1 { self.last_pt_held.0 = false; }
+    else if pt1_press && !excluded { self.last_pt_held.0 = true; }
+    if !pt2 { self.last_pt_held.1 = false; }
+    else if pt2_press && !excluded { self.last_pt_held.1 = true; }
+    // Return if in the excluded range and not dragging
+    if excluded && (!last_pt1_held && !last_pt2_held) { return; }
+
+    let (pt1_held, pt2_held) = self.last_pt_held;
+
+    // Process events
+    if pt1_press { println!("press"); }
+    if pt1_rel { println!("release"); }
+    painter.add(egui::Shape::circle_filled(pt_pos, 10.0,
+      if pt1_held { egui::Color32::from_rgb(255, 128, 128) }
+      else { egui::Color32::from_rgb(255, 255, 128) }));
   }
 }
 
@@ -105,15 +145,21 @@ impl epi::App for App {
     egui::CentralPanel::default().show(ctx, |ui| {
       let rect_painter = ui.available_rect_before_wrap();
       let painter = egui::Painter::new(
-        ui.ctx().clone(), ui.layer_id(), rect_painter);
+        ui.ctx().clone(),
+        // egui::layers::LayerId::new(egui::layers::Order::Foreground, egui::Id::new("1111")),
+        ui.layer_id(),
+        rect_painter);
       painter.add(egui::Shape::rect_filled(
         rect_painter, 6.0, egui::Color32::from_rgba_premultiplied(8, 8, 8, 16)));
       painter.add(egui::Shape::circle_filled(
         rect_painter.center(), 480.0, egui::Color32::from_rgb(64, 128, 255)));
+
       self.process_canvas_interactions(
         painter,
         rect_painter,
-        ui.allocate_rect(rect_painter, egui::Sense::click_and_drag()),
+        self.last_rect_popup,
+        ui.allocate_rect(rect_painter, egui::Sense::hover()),
+        ui.input(),
       );
 
       let rect_popup = egui::Rect {
@@ -150,6 +196,7 @@ impl epi::App for App {
                 egui::Button::new("☆ new"));
             });
           });
+        self.last_rect_popup = ui.min_rect();
       });
     });
   }
