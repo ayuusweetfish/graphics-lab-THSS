1cf7fc0e047042d5ae9cc82838ee4decd651c390
Shiqing <shiqing-thu18@yandex.com>
2021-09-27 01:08:11 +0800

    Add and remove polygons

--- src/app.rs
+++ src/app.rs
@@ -3,9 +3,20 @@ use eframe::{egui, epi};
 #[derive(Debug)]
 #[cfg_attr(feature = "persistence", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "persistence", serde(default))]
+pub struct Polygon {
+  khroma: [f32; 3],
+  points: Vec<(f32, f32)>,
+}
+
+#[derive(Debug)]
+#[cfg_attr(feature = "persistence", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "persistence", serde(default))]
 pub struct App {
   dark: bool,
-  polygon_khroma: [f32; 3],
+
+  polygons: Vec<Polygon>,
+  sel_polygon: Option<usize>,
+  polygons_collapsed: bool,
 
   last_rect_popup: egui::Rect,
   last_pt_down: (bool, bool),
@@ -14,14 +25,19 @@ pub struct App {
 
 impl Default for App {
   fn default() -> Self {
-    Self {
+    let mut result = Self {
       dark: false,
-      polygon_khroma: [0f32; 3],
+
+      polygons: vec![],
+      sel_polygon: None,
+      polygons_collapsed: false,
 
       last_rect_popup: egui::Rect::NOTHING,
       last_pt_down: (false, false),
       last_pt_held: (false, false),
-    }
+    };
+    result.add_polygon();
+    result
   }
 }
 
@@ -33,6 +49,14 @@ impl App {
     );
   }
 
+  fn add_polygon(&mut self) {
+    self.polygons.push(Polygon {
+      khroma: [0.5, 0.5, 1.0],
+      points: vec![],
+    });
+    self.sel_polygon = Some(self.polygons.len() - 1);
+  }
+
   fn process_canvas_interactions(
     &mut self,
     painter: egui::Painter,
@@ -41,6 +65,18 @@ impl App {
     resp: egui::Response,
     input: &egui::InputState,
   ) {
+    // Draw polygons
+    // TODO
+    if self.sel_polygon.is_some() && !self.polygons_collapsed {
+      painter.add(egui::Shape::circle_filled(
+        rect.center(), 480.0, egui::Color32::from_rgb(64, 128, 255)));
+    } else {
+      painter.add(egui::Shape::circle_stroke(
+        rect.center(), 480.0,
+        egui::Stroke::new(6.0, egui::Color32::from_rgb(64, 128, 255))));
+    }
+
+    // Pointer interactions
     let pt_pos = match input.pointer.hover_pos().or(input.pointer.interact_pos()) {
       Some(p) => p,
       None => return, // Return if no position information is available
@@ -151,8 +187,6 @@ impl epi::App for App {
         rect_painter);
       painter.add(egui::Shape::rect_filled(
         rect_painter, 6.0, egui::Color32::from_rgba_premultiplied(8, 8, 8, 16)));
-      painter.add(egui::Shape::circle_filled(
-        rect_painter.center(), 480.0, egui::Color32::from_rgb(64, 128, 255)));
 
       self.process_canvas_interactions(
         painter,
@@ -179,22 +213,51 @@ impl epi::App for App {
               self.dark = !self.dark;
               self.update_theme(ctx);
             }
-            egui::CollapsingHeader::new("Polygons").default_open(true).show(ui, |ui| {
-              for i in 0..3 {
+            if egui::CollapsingHeader::new("Polygons").default_open(true).show(ui, |ui| {
+              let mut polygon_remove = None;
+              for (index, poly) in self.polygons.iter_mut().enumerate() {
+                let sel = match self.sel_polygon {
+                  Some(i) => i == index,
+                  _ => false,
+                };
                 ui.horizontal(|ui| {
-                  ui.add_sized(
+                  if ui.add_sized(
                     egui::vec2(80.0, ui.style().spacing.interact_size.y),
-                    egui::SelectableLabel::new(i == 2, format!("#{} (10)", i + 1)));
-                  ui.color_edit_button_rgb(&mut self.polygon_khroma);
-                  if i == 2 {
-                    ui.selectable_label(false, "×");
+                    egui::SelectableLabel::new(sel, format!("#{} (10)", index + 1))
+                  ).clicked() {
+                    self.sel_polygon = if sel { None } else { Some(index) };
+                  }
+                  ui.color_edit_button_rgb(&mut poly.khroma);
+                  if sel {
+                    if ui.selectable_label(false, "×").clicked() {
+                      polygon_remove = Some(index);
+                    }
                   }
                 });
               }
-              ui.add_sized(
+              if let Some(index) = polygon_remove {
+                self.polygons.remove(index);
+                if let Some(i) = self.sel_polygon {
+                  if i > index {
+                    self.sel_polygon = Some(i - 1);
+                  } else if i == index {
+                    self.sel_polygon = None;
+                  }
+                } else {
+                  self.sel_polygon = None;
+                }
+              }
+              if ui.add_sized(
                 egui::vec2(158.0, ui.style().spacing.interact_size.y),
-                egui::Button::new("☆ new"));
-            });
+                egui::Button::new("☆ new")).clicked()
+              {
+                self.add_polygon();
+              }
+            }).body_response.is_none() {
+              self.polygons_collapsed = true;
+            } else {
+              self.polygons_collapsed = false;
+            }
           });
         self.last_rect_popup = ui.min_rect();
       });
