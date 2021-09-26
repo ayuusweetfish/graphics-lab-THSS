446f21c60233475fd909a7b3b64cdc0c48813361
Shiqing <shiqing-thu18@yandex.com>
2021-09-26 16:33:53 +0800

    Popup GUI

--- src/app.rs
+++ src/app.rs
@@ -4,14 +4,25 @@ use eframe::{egui, epi};
 #[cfg_attr(feature = "persistence", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "persistence", serde(default))]
 pub struct App {
-  x: f32,
+  dark: bool,
+  polygon_khroma: [f32; 3],
 }
 
 impl Default for App {
   fn default() -> Self {
     Self {
-      x: 0.0,
+      dark: false,
+      polygon_khroma: [0f32; 3],
     }
+  }
+}
+
+impl App {
+  fn update_theme(&self, ctx: &egui::CtxRef) {
+    ctx.set_visuals(
+      if self.dark { egui::style::Visuals::dark() }
+      else { egui::style::Visuals::light() }
+    );
   }
 }
 
@@ -49,10 +60,16 @@ impl epi::App for App {
     style.spacing.window_padding = egui::vec2(8.0, 8.0);
     style.spacing.button_padding = egui::vec2(5.0, 2.5);
     style.spacing.slider_width = 40.0;
+    style.spacing.interact_size.y =
+      // ctx.fonts().layout_single_line(egui::TextStyle::Button, "A".into()).size.y +
+      16.0 +
+      style.spacing.button_padding.y * 2.0;
     style.visuals.popup_shadow = egui::epaint::Shadow {
       extrusion: 6.0, color: egui::Color32::from_black_alpha(32)
     };
     ctx.set_style(style);
+
+    self.update_theme(ctx);
 
     #[cfg(feature = "persistence")]
     if let Some(storage) = storage {
@@ -72,7 +89,49 @@ impl epi::App for App {
 
   fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
     egui::CentralPanel::default().show(ctx, |ui| {
-      ui.add(egui::DragValue::new(&mut self.x).clamp_range(0.0..=10.0).speed(0.1));
+      let rect_painter = ui.available_rect_before_wrap();
+      let painter = egui::Painter::new(
+        ui.ctx().clone(), ui.layer_id(), rect_painter);
+      painter.add(egui::Shape::rect_filled(
+        rect_painter, 6.0, egui::Color32::from_rgba_premultiplied(8, 8, 8, 16)));
+      painter.add(egui::Shape::circle_filled(
+        rect_painter.center(), 480.0, egui::Color32::from_rgb(64, 128, 255)));
+
+      let rect_popup = egui::Rect {
+        min: egui::pos2(16.0, 16.0),
+        max: egui::pos2(16.0, 16.0),
+      };
+      let width = ui.available_width();
+      ui.allocate_ui_at_rect(rect_popup, |ui| {
+        egui::Frame::popup(ui.style())
+          .fill(
+            if self.dark { egui::Color32::from_rgba_premultiplied(32, 32, 32, 216) }
+            else { egui::Color32::from_rgba_premultiplied(216, 216, 216, 216) }
+          )
+          .show(ui, |ui| {
+            ui.set_max_width(width);
+            if ui.button("dark/light").clicked() {
+              self.dark = !self.dark;
+              self.update_theme(ctx);
+            }
+            egui::CollapsingHeader::new("Polygons").default_open(true).show(ui, |ui| {
+              for i in 0..3 {
+                ui.horizontal(|ui| {
+                  ui.add_sized(
+                    egui::vec2(80.0, ui.style().spacing.interact_size.y),
+                    egui::SelectableLabel::new(i == 2, format!("#{} (10)", i + 1)));
+                  ui.color_edit_button_rgb(&mut self.polygon_khroma);
+                  if i == 2 {
+                    ui.selectable_label(false, "×");
+                  }
+                });
+              }
+              ui.add_sized(
+                egui::vec2(158.0, ui.style().spacing.interact_size.y),
+                egui::Button::new("☆ new"));
+            });
+          });
+      });
     });
   }
 }
