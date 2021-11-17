2c295d533b2138e691b9e782493d7ead5192333a
Shiqing <shiqing-thu18@yandex.com>
2021-11-17 16:01:20 +0800

    Rotation

--- particles/1.py
+++ particles/1.py
@@ -21,8 +21,9 @@ M = 11
 bodyIdx = ti.Vector.field(2, int, (M,))   # (start, end)
 bodyPos = ti.Vector.field(3, float, (M,))
 bodyVel = ti.Vector.field(3, float, (M,))
+bodyIne = ti.Matrix.field(3, 3, float, (M,))
 bodyAng = ti.Vector.field(3, float, (M,))
-bodyOri = ti.Vector.field(4, float, (M,)) # quaternion
+bodyOri = ti.Vector.field(4, float, (M,)) # quaternion, (x, y, z, w)
 
 count = ti.field(int, ())
 
@@ -39,7 +40,32 @@ def init():
     bodyIdx[i] = ti.Vector([i * 3, i * 3 + 3])
     bodyVel[i] = ti.Vector([-0.2, ti.random() * 0.5, 0])
     bodyAng[i] = ti.Vector([0, 0, 0])
-    bodyOri[i] = ti.Vector([0, 0, 0, 0])
+    bodyOri[i] = ti.Vector([0, 0, 0, 1])
+
+@ti.func
+def quat_mul(a, b):
+  av = a.xyz
+  bv = b.xyz
+  rv = a.w * bv + b.w * av + av.cross(bv)
+  w = a.w * b.w - av.dot(bv)
+  return ti.Vector([rv.x, rv.y, rv.z, w])
+
+@ti.func
+def quat_rot(v, q):
+  return quat_mul(quat_mul(
+    q,
+    ti.Vector([ v.x,  v.y,  v.z, 0])),
+    ti.Vector([-q.x, -q.y, -q.z, q.w])
+  ).xyz
+
+@ti.func
+def quat_mat(q):
+  x, y, z, w = q.x, q.y, q.z, q.w
+  return ti.Matrix([
+    [1 - 2*y*y - 2*z*z, 2*x*y - 2*w*z, 2*x*z + 2*w*y],
+    [2*x*y + 2*w*z, 1 - 2*x*x - 2*z*z, 2*y*z - 2*w*x],
+    [2*x*z - 2*w*y, 2*y*z + 2*w*x, 1 - 2*x*x - 2*y*y]
+  ])
 
 @ti.func
 def step():
@@ -50,8 +76,9 @@ def step():
   # Calculate particle position and velocity
   for i in range(M):
     for j in range(bodyIdx[i][0], bodyIdx[i][1]):
-      x[j] = bodyPos[i] + x0[j]
-      v[j] = bodyVel[i]
+      r = quat_rot(x0[j], bodyOri[i])
+      x[j] = bodyPos[i] + r
+      v[j] = bodyVel[i] + bodyAng[i].cross(r)
 
   # Force
   for i in range(M): f[i] = ti.Vector([0, 0, 0])
@@ -79,15 +106,27 @@ def step():
 
   # Integrate
   for i in range(M):
-    bodyPos[i] += bodyVel[i] * dt
     fSum = ti.Vector([0.0, 0.0, 0.0])
+    tSum = ti.Vector([0.0, 0.0, 0.0])
     # for j in range(bodyIdx[i][0], bodyIdx[i][1]):
     # Crash?
     j = bodyIdx[i][0]
     while j < bodyIdx[i][1]:
       fSum += f[j]
+      tSum += (x[j] - bodyPos[i]).cross(f[j])
       j += 1
     bodyVel[i] += fSum * 0.2 * dt
+    bodyPos[i] += bodyVel[i] * dt
+    # Update angular momentum
+    bodyAng[i] += tSum * dt
+    rotMat = quat_mat(bodyOri[i])
+    angVel = (rotMat * bodyIne[i] * rotMat.transpose()).__matmul__(bodyAng[i])
+    if bodyAng[i].norm() >= 1e-5:
+      theta = bodyAng[i].norm() * dt
+      dqw = ti.cos(theta / 2)
+      dqv = ti.sin(theta / 2) * bodyAng[i].normalized()
+      dq = ti.Vector([dqv.x, dqv.y, dqv.z, dqw])
+      bodyOri[i] = quat_mul(dq, bodyOri[i])
 
 @ti.kernel
 def frame():
