519aa6301ab3378a34d21fa91f51a0373804bc66
Shiqing <shiqing-thu18@yandex.com>
2021-11-17 12:11:18 +0800

    Collisions

--- particles/1.py
+++ particles/1.py
@@ -2,25 +2,39 @@ import taichi as ti
 import taichi_glsl as ts
 ti.init(arch=ti.vulkan)
 
-dt = 1.0 / 60
+dt = 1.0 / 600
 R = 0.1
-G = 0.2
+G = 0.4
+
+K = 100
 
 N = 11
 x = ti.Vector.field(3, float, (N,))
 v = ti.Vector.field(3, float, (N,))
+f = ti.Vector.field(3, float, (N,))
 
 @ti.kernel
 def init():
   for i in range(N):
-    x[i] = ti.Vector([-0.5 + R * i, 0, 0])
+    x[i] = ti.Vector([-0.5 + R * 0.9 * i, R * 0.2 * i, 0])
     v[i] = ti.Vector([0, ti.random() * 0.5, 0])
 
-@ti.kernel
+@ti.func
 def step():
   # Gravity
   for i in range(N):
     v[i].y -= G * dt
+  # Collision
+  for i in range(N):
+    f[i] = ti.Vector([0, 0, 0])
+    for j in range(N):
+      if i == j: continue
+      d = (x[i] - x[j]).norm()
+      if d < R:
+        # Repulsive
+        f[i] += K * (R * 2 - d) * (x[i] - x[j]).normalized()
+  for i in range(N):
+    v[i] += f[i] * 0.2 * dt
   # Integrate
   for i in range(N):
     x[i] += v[i] * dt
@@ -34,6 +48,11 @@ def step():
       v[i].z *= -1
       x[i].z = ts.clamp(x[i].z, -0.8 + R, 0.8 - R)
 
+@ti.kernel
+def frame():
+  for i in range(10):
+    step()
+
 init()
 
 window = ti.ui.Window('Collision', (600, 600), vsync=True)
@@ -42,7 +61,7 @@ scene = ti.ui.Scene()
 camera = ti.ui.make_camera()
 
 while window.running:
-  step()
+  frame()
 
   camera.position(0, 0, 2)
   camera.lookat(0, 0, 0)
